8b4b2c959f4ef3499f3d672a158f9b75ecd56020
jagLas <jglasher@gmail.com>
2022-12-07 12:13:47 -0500

    added movement messages

--- starter/class/enemy.js
+++ starter/class/enemy.js
@@ -7,7 +7,7 @@ class Enemy extends Character {
     this.cooldown = 0;
     this.attackTarget = null;
     //line below can be commented to turn on and off enemy behavior
-    this.act();
+    this.rest();
   }
 
   setPlayer(player) {
@@ -15,13 +15,36 @@ class Enemy extends Character {
   }
 
   randomMove() {
+    //creates available movements commands
     let exits = this.currentRoom.exits;
     exits = Object.keys(exits);
-    // console.log(exits)
+
+     //randomly chooses from exits
     let direction = exits[Math.floor(Math.random() * exits.length)]
-    // console.log(this.currentRoom.getRoomInDirection(direction))
-    this.currentRoom = this.currentRoom.getRoomInDirection(direction)
-    this.cooldown = 3000;
+
+    //alerts the player it left the room and which direction
+    if (this.player && this.player.currentRoom === this.currentRoom) {
+      let dirName;
+      const dirNames = ['north', 'south', 'east', 'west']
+
+      dirNames.forEach(dir => {
+        if (dir.startsWith(direction)) {
+          dirName = dir;
+        }
+      })
+
+      console.log(`The ${this.name} left the room going ${dirName}.`);
+    }
+
+    //moves in the random direction
+    this.currentRoom = this.currentRoom.getRoomInDirection(direction);
+
+    //tells the player if the enemy enters their room
+    if (this.player && this.player.currentRoom === this.currentRoom) {
+      console.log(`The ${this.name} enters the room.`)
+    }
+
+    this.rest();
   }
 
   takeSandwich() {
@@ -42,14 +65,13 @@ class Enemy extends Character {
     const resetCooldown = function() {
       this.cooldown = 0;
       this.act();
-      
     };
 
     setTimeout(resetCooldown.bind(this), this.cooldown);
   }
 
   attack() {
-    if (this.player && this.player.currentRoom === this.currentRoom) {
+    if (this.player && this.player.currentRoom === this.currentRoom) { //checks if player is in the room
       this.attackTarget.applyDamage(this.strength);
       console.log(`You have been hit for ${this.strength} damage!`)
     }
@@ -60,13 +82,12 @@ class Enemy extends Character {
     if (this.health <= 0) {
       // Dead, do nothing;
     } else if (!this.attackTarget) { //checks if it's targeting player
-      this.scratchNose();
-      this.rest();
+      // this.scratchNose();
+      this.randomMove();
+      // this.rest();
     } else {
       this.attack(this.attackTarget); //attacks the player
     }
-
-    // Fill this in
   }
 
   scratchNose() {
